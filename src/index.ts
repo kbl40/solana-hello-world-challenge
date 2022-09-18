437250f2810db86da0deeb7e76a5251dae35d06f
Kyle B. Lackinger <67078189+kbl40@users.noreply.github.com>
2022-09-18 12:01:54 -0400

    transaction logic added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,44 @@
 import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
 
+async function sagHallo(
+   connection: web3.Connection,
+   payer: web3.Keypair
+): Promise<web3.TransactionSignature> {
+  const transaction = new web3.Transaction()
+
+  const instruction = new web3.TransactionInstruction(
+    {
+      keys: [],
+      programId: new web3.PublicKey('CUnSKSqXShFhZQLTnt53ZzNps3CYqJX57XSEB9XN8tX3')
+    }
+  )
+
+  transaction.add(instruction)
+
+  const txSig = await web3.sendAndConfirmTransaction(
+    connection, 
+    transaction,
+    [payer]
+  )
+
+  console.log(
+    `Transaction: https://explorer.solana.com/tx/${txSig}?cluster=devnet`
+  )
+  
+  return txSig
+}
+
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
   const user = await initializeKeypair(connection)
 
   console.log("PublicKey:", user.publicKey.toBase58())
+
+  await sagHallo(
+    connection,
+    user
+  )
 }
 
 main()
